b4c4e3a89706169497825592b2b2df7af36cb07d
Bruna Almeida <bru.csa16@gmail.com>
2025-05-07 07:21:56 -0300

    JS atualização

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-alert ('Olá')
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert(`Por favor, digite um número!`)
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ``
+        //`` - Recebe nada, limpa o campo
+        while (c <= 10) {
+            let item = document.createElement('option')
+            //createElement: Criar um elemento no HTML dentro do option
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            //appendChild: Add elemento filho
+            c++
+        }
+    }
+}
